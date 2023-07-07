77a9fdc60c9b320005b2d21556cfc055df9d7214
YDS！ <1023571809@qq.com>
2023-07-07 11:42:12 +0800

    项目复制

---
+++ quantum_code/quantum.code/RuntimeConfig.User.cs
@@ -0,0 +1,15 @@
+﻿using Photon.Deterministic;
+using System;
+
+namespace Quantum
+{
+    partial class RuntimeConfig
+    {
+        public AssetRefEntityPrototype CharacterPrototype;
+
+        partial void SerializeUserData(BitStream stream)
+        {
+            stream.Serialize(ref CharacterPrototype.Id);
+        }
+    }
+}
